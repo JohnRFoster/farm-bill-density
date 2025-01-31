78a655d9fa6cea95c2c6390d17ec5b73d249ae5c
JohnRFoster <john.foster@usda.gov>
2025-01-31 16:59:01 -0600

    modify

--- slurm_run_simulation.sh
+++ slurm_run_simulation.sh
@@ -3,9 +3,9 @@
 #SBATCH --nodes=1                       # default number of nodes
 #SBATCH --partition=cpu_compute         # good enough for what I need
 #SBATCH --cpus-per-task=5               # for a multithredded job
-#SBATCH --mem=96g                       # memory
-#SBATCH --job-name=clusterSim             # job name
-#SBATCH --output=outfiles/simulation_%J.txt    # output file
+#SBATCH --mem=24g                       # memory
+#SBATCH --job-name=farmBillSim             # job name
+#SBATCH --output=outfiles/farmBillSim_%a.txt    # output file
 
 module add R
-Rscript workflow.R
+Rscript workflow.R $SLURM_ARRAY_TASK_ID
